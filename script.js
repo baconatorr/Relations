4043ce1c6a234f4bf9742d14c26bfbc42f01a836
Mason Williams <154944752+baconatorr@users.noreply.github.com>
2024-05-16 09:33:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -99,12 +99,12 @@ function loadGrid() {
 function activateLetter(tile) {
     if (tile.getAttribute("data-active") == "true") {
         if(tile == lastClickedTile){
+            tile.setAttribute("data-active", "false");
             current.pop();
             currentIds.pop();
             current = current.length > 0 ? current : ["Enter a word"]
             document.querySelector(".letter-display").innerText = current.join('');
             tile.classList.remove("tile-active");
-            tile.setAttribute("data-active", "false");
             clicked.pop();
             lastClickedTile = clicked.length > 0 ? clicked[clicked.length - 1] : null
         }
